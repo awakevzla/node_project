b15e0ff1ad7c55846bdc02ffb60dd1b8c3285fed
Pedro Lugo <robertty55@gmail.com>
2016-05-08 18:05:32 -0430

    COmienzo de sessiones

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var validator = require('express-validator');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -19,8 +21,10 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret:'123', saveUninitialized:false,resave:false}));
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Validaciones', success:false, errors:req.session.errors });
+  req.session.errors=null;
 });
-
-router.get('/test/:id', function(req, res, next) {
-  res.render('test', { title: 'Express', id:req.params.id });
-});
-router.post('/test/submit', function (req, res, next) {
-  var id = req.body.id;
-  res.redirect('/test/'+id);
+router.post('/submit', function (req, res, next) {
+  //validando
+  req.check('email', 'Invalid Email Address').isEmail();
+  req.check('password', 'Password must have 4 characters at least').isLength({min:4});
+  req.check('password', 'Password cant have more than 12 characters').isLength({max:12});
+  req.check('password', 'Confirmation Error').equals(req.body.passwordConfirm);
+  var errors = req.validationErrors();
+  if(errors){
+    req.session.errors=errors;
+    console.log(errors);
+  }
+  res.redirect('/');
 });
 module.exports = router;
